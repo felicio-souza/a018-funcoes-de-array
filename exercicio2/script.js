c86db41336427e326f8155b3ccac467707797e8a
felicio-souza <felicio.fc13@gmail.com>
2022-08-25 14:29:48 -0300

    Trabalho realizado

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+const arrayNumeros = [12, 22, 78, 45, 2, 3, 8, 10]
+
+const triplos = arrayNumeros.map((numeros) => {
+
+return numeros * 3
+   })
+console.log(triplos)
+
+
+const metades = arrayNumeros.map((numeros) => {
+
+    return numeros / 2
+       })
+    console.log(metades)
